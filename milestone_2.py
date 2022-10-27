d989b0d00802936e8a77b710ec71687b7deaabbc
Yurirtp <116115861+Yurirtp@users.noreply.github.com>
2022-10-27 21:29:19 +0100

    Update milestone_2.py

---
+++ milestone_2.py
@@ -0,0 +1,16 @@
+#%%
+import random 
+
+
+word_list = ["Pear", "Pineapple", "Apple", "Mango", "Melon"]
+
+word = random.choice(word_list)
+
+print(word)
+
+guess = input("Enter a single letter.")
+
+if len(guess) == 1 and guess.isalpha():
+    print("Good guess!")
+else: 
+    print("Oops! That is not a valid input")
