51093900b49ac8c35e9a8e03df9a1d592d0bd877
Yurirtp <116115861+Yurirtp@users.noreply.github.com>
2022-10-31 22:38:30 +0000

    Update milestone_3.py

--- milestone_3.py
+++ milestone_3.py
@@ -1,47 +1,43 @@
 # %%
-import random
+import random 
 
-
-
+guess = input("Enter a single letter.")
 
 word_list = ["Pear", "Pineapple", "Apple", "Mango", "Melon"]
 
 word = random.choice(word_list)
 num_lives = 5
 
-def check_guess():
-    guess = input("Enter a single pletter.")
-    guess = guess.lower()  
-    while True:
-        if guess in word:
-            print(f"Good guess! {guess} is in the word.")
-            break
-        else:
-            print(f"Sorry, {guess} is not in the word. Try again.")
-            break
+class Hangman:
+    def __init__(self, word_list, num_lives = 5):
+        self.word_list = word_list
+        self.word = random.choice(word_list)
+        self.guessed = [] #making an empty list to then append later 
+        self.lives = num_lives
+        
 
-def ask_for_input():
-    check = check_guess()
-    while True:
-        guess = input("Enter a single letter.")
-        if len(guess) == 1 and guess.isalpha():
-            break
-        else: 
-            print("Invalid letter. Please, enter a single alphabetical character.")
-            break
-    check_guess()
+    def check_guess(self, guess):
+        guess = guess.lower()  
+        while True:
+                if guess in word:
+                    print(f"Good guess! {guess} is in the word.")
+                    break
+                else:
+                    print(f"Sorry, {guess} is not in the word. Try again.")
+                    break 
 
+    def ask_for_input(self):
+        check = self.check_guess
+        while True:
+                guess = input("Enter a single letter.")
+                if len(guess) == 1 and guess.isalpha():
+                    break
+                else: 
+                    print("Invalid letter. Please, enter a single alphabetical character.")
+                    break
+        self.check_guess(guess)
+            
+a = Hangman([word_list, num_lives])
 
-
-ask_for_input()
-
-
-
-
-
-                
-
-
-
-
+a.ask_for_input()
 
