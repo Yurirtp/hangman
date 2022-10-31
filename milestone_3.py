cb4c2df216c1d29d8dd26298306b836184c705d0
Yurirtp <116115861+Yurirtp@users.noreply.github.com>
2022-10-31 21:39:19 +0000

    Update milestone_3.py

--- milestone_3.py
+++ milestone_3.py
@@ -9,14 +9,7 @@ word_list = ["Pear", "Pineapple", "Apple", "Mango", "Melon"]
 word = random.choice(word_list)
 num_lives = 5
 
-class Hangman:
-    def __init__(self, word_list, num_lives):
-        self.word_list = word_list
-        self.word = random.choice(word_list)
-        self.guessed = [] #making an empty list to then append later 
-        self.num_lives = num_lives
-
-    def check_guess(self):
+def check_guess():
             guess = input("Enter a single letter.")
             guess = guess.lower()  
             while True:
@@ -27,8 +20,8 @@ class Hangman:
                         print(f"Sorry, {guess} is not in the word. Try again.")
                         break 
 
-    def ask_for_input(self):
-        check = self.check_guess
+def ask_for_input():
+        check = check_guess
         while True:
             guess = input("Enter a single letter.")
             if len(guess) == 1 and guess.isalpha():
@@ -38,10 +31,10 @@ class Hangman:
                 break
             
         return check()
-            
-a = Hangman(word_list, num_lives)
 
-a.ask_for_input()
+
+
+ask_for_input()
 
 
 
