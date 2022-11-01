1261870b358aeab9d2710b41dc9ef596c7387d30
Yurirtp <116115861+Yurirtp@users.noreply.github.com>
2022-11-01 22:12:23 +0000

    Create milestone_5.py

---
+++ milestone_5.py
@@ -0,0 +1,60 @@
+#%%
+import random
+guess = input("Enter a single letter.")
+
+word_list = ["Pear", "Pineapple", "Apple", "Mango", "Melon"]
+
+word = random.choice(word_list)
+num_lives = 5
+
+class Hangman:
+    def __init__(self, word_list, num_lives):
+        self.word_list = word_list
+        self.num_lives = num_lives
+        self.word = random.choice(word_list)
+        self.list_of_guesses = [] #making an empty list to then append later 
+        self.num_lives = 5
+        self.guess = guess
+        self.word_guessed = len(self.word) * ["_"]
+        self.num_letters = len(set(self.word).difference(set(self.word_guessed)))
+        
+
+    def check_guess(self, guess):
+        guess = guess.lower()  
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+
+            for i, letter in enumerate(self.word):
+                if letter == guess:
+                    self.word_guessed[i] = guess
+                    print(self.word_guessed)
+                self.num_letters = self.num_letters-1     
+        else:
+            self.num_lives -=1
+            print(f"Sorry, {guess} is not in the word. Try again.") 
+            print(f"You have {num_lives} lives left.")    
+        self.list_of_guesses.extend(guess)        
+
+
+    def ask_for_input(self):
+        while True:
+                guess = input("Enter a single letter.")
+                if len(guess) != 1 and guess.isalpha() == False:
+                    print("Invalid letter. Please, enter a single alphabetical character.")
+                    break
+                elif guess in self.list_of_guesses:
+                    print("You already tried that letter!")
+                    break
+                else:
+                    self.list_of_guesses.extend(guess)
+                    self.check_guess(guess)                    
+                    break
+
+    
+            
+run = Hangman(word_list,num_lives)
+run.ask_for_input()
+
+
+
+
