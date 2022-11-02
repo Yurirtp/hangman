b2215226c34ada315955024dab73f5f65401c32b
Yurirtp <116115861+Yurirtp@users.noreply.github.com>
2022-11-02 16:58:09 +0000

    Update milestone_3.py

--- milestone_3.py
+++ milestone_3.py
@@ -15,7 +15,7 @@ def check_guess(guess):
         print(f"Sorry, {guess} is not in the word. Try again.")
 
 
-def ask_for_input(guess):
+def ask_for_input():
     while True:
         guess = input("Enter a single letter.")
         guess = guess.lower() 
@@ -25,7 +25,7 @@ def ask_for_input(guess):
             print("Invalid letter. Please, enter a single alphabetical character.")
     check_guess(guess)
             
-ask_for_input(guess)
+ask_for_input()
 
 
 # %%
