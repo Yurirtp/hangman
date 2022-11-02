64a2f261cc151846cb3272ecbacbb21042b47786
Yurirtp <116115861+Yurirtp@users.noreply.github.com>
2022-11-02 21:16:35 +0000

    Update milestone_4.py

--- milestone_4.py
+++ milestone_4.py
@@ -13,7 +13,6 @@ class Hangman:
     def check_guess(self, guess): # need to copy milestone 3 set up 
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
-            self.list_of_guesses.append(guess) 
             for i, letter in enumerate(self.word):
                 if letter == guess:
                     self.word_guessed[i] = letter     
@@ -21,9 +20,7 @@ class Hangman:
         else:
             self.num_lives -=1
             print(f"Sorry, {guess} is not in the word. Try again.") 
-            print(f"You have {self.num_lives} lives left.")    
-            
-        self.list_of_guesses.append(guess)        
+            print(f"You have {self.num_lives} lives left.")          
 
 
     def ask_for_input(self): 
