5e19864ac325f5bdf97ce45e1e910cfc34a54d22
Yurirtp <116115861+Yurirtp@users.noreply.github.com>
2022-11-02 20:16:50 +0000

    Update milestone_4.py

--- milestone_4.py
+++ milestone_4.py
@@ -18,7 +18,7 @@ class Hangman:
             for i, letter in enumerate(self.word):
                 if letter == guess:
                     self.word_guessed[i] = guess
-                self.num_letters = self.num_letters-1     
+                    self.num_letters -=1     
         else:
             self.num_lives -=1
             print(f"Sorry, {guess} is not in the word. Try again.") 
@@ -36,7 +36,7 @@ class Hangman:
                 elif guess in self.list_of_guesses:
                     print("You already tried that letter!")
                 else:
-                    self.list_of_guesses.extend(guess)
+                    self.list_of_guesses.append(guess)
                     self.check_guess(guess)                    
                     break
             
