456236a3f540475ba49f286cb3ce4b3c80712d63
Yurirtp <116115861+Yurirtp@users.noreply.github.com>
2022-11-02 21:28:35 +0000

    Update milestone_4.py

--- milestone_4.py
+++ milestone_4.py
@@ -18,10 +18,11 @@ class Hangman:
                     self.word_guessed[i] = letter     
             self.num_letters -=1     
         else:
+            letter = guess
             self.num_lives -=1
-            print(f"Sorry, {guess} is not in the word") 
+            print(f"Sorry, {letter} is not in the word.") 
             print(f"You have {self.num_lives} lives left.") 
-            
+
         self.list_of_guesses.append(guess)             
 
 
