eed3e445aa897fda4ea916f2749b11f454a9a030
Yurirtp <116115861+Yurirtp@users.noreply.github.com>
2022-11-02 16:50:50 +0000

    Update milestone_3.py

--- milestone_3.py
+++ milestone_3.py
@@ -8,14 +8,14 @@ word_list = ["pear", "pineapple", "apple", "mango", "melon"]
 word = random.choice(word_list)
 num_lives = 5
    
-def check_guess(): 
+def check_guess(guess): 
     if guess in word:
         print(f"Good guess! {guess} is in the word.")
     else:
         print(f"Sorry, {guess} is not in the word. Try again.")
 
 
-def ask_for_input():
+def ask_for_input(guess):
     while True:
         guess = input("Enter a single letter.")
         guess = guess.lower() 
@@ -23,9 +23,9 @@ def ask_for_input():
             break
         else: 
             print("Invalid letter. Please, enter a single alphabetical character.")
-    check_guess()
+    check_guess(guess)
             
-ask_for_input()
+ask_for_input(guess)
 
 
 # %%
