1c2fb5d1359f747076c65d88856497f72219bba8
Yurirtp <116115861+Yurirtp@users.noreply.github.com>
2022-11-02 20:21:10 +0000

    Update milestone_4.py

--- milestone_4.py
+++ milestone_4.py
@@ -18,7 +18,8 @@ class Hangman:
             for i, letter in enumerate(self.word):
                 if letter == guess:
                     self.word_guessed[i] = guess
-                    self.num_letters -=1     
+                    
+            self.num_letters -=1     
         else:
             self.num_lives -=1
             print(f"Sorry, {guess} is not in the word. Try again.") 
