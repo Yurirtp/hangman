1d6fb862de8398fd36bd5b26cdbc029da87b8301
Yurirtp <116115861+Yurirtp@users.noreply.github.com>
2022-11-03 18:38:44 +0000

    Update milestone_5.py

--- milestone_5.py
+++ milestone_5.py
@@ -2,7 +2,7 @@
 import random
 
 class Hangman:
-    def __init__(self, word_list, num_lives=5):
+    def __init__(self, word_list, num_lives):
         self.word_list = word_list
         self.num_lives = num_lives
         self.word = random.choice(word_list)
